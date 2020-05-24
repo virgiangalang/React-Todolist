5c55feea7bfd68adc8012ce738abc7247d04943d
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-05-24 21:47:06 +0700

    Komponen Header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from "react";
 import "./styles.css";
 import Paper from "./components/Paper";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <Paper>
-      <section className="header">
-        <button className="header-btn main-black-color">Add</button>
-        <h1 className="header-title">Todo Lists</h1>
-        <button className="header-btn main-red-color">Clear</button>
-      </section>
+      <Header />
 
       <section className="add">
         <form className="add-form">
