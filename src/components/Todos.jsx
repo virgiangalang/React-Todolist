1e5b7239904a1b019cf71dbd8092995269ab37de
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-05-25 20:51:15 +0700

    Refactor todo dan todos component

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,39 +1,8 @@
 import React from "react";
 import Todo from "./Todo";
+import PropTypes from "prop-types";
 
-const Todos = () => {
-  const todos = [
-    {
-      text: "Belajar React;"
-    },
-    {
-      text: "Belajar Mencintaimu;"
-    },
-    {
-      text: "Belajar Menyukaimu;"
-    },
-    {
-      text: "Belajar Hidup;"
-    },
-    {
-      text: "Belajar Bernafas;"
-    },
-    {
-      text: "Belajar Makan;"
-    },
-    {
-      text: "Belajar React;"
-    },
-    {
-      text: "Belajar React;"
-    },
-    {
-      text: "Belajar React;"
-    },
-    {
-      text: "Belajar React;"
-    }
-  ];
+const Todos = ({ todos }) => {
   return (
     <section className="todos">
       {todos.map(todo => {
@@ -42,5 +11,11 @@ const Todos = () => {
     </section>
   );
 };
+
+Todos.PropTypes ={
+  todos: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string;
+  }))
+}
 
 export default Todos;
