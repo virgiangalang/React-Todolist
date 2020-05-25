e2a35948b1a63d4f4f482d9785fa0b136976a147
virgiangalang <65715655+virgiangalang@users.noreply.github.com>
2020-05-25 21:27:11 +0700

    Membuat add todo di todolist page

--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -11,10 +11,19 @@ const TodoList = () => {
     { text: "Learning React Hooks" },
     { text: "Learning styling in React" }
   ]);
+
+  const addTodo = value => {
+    const addedTodo = [...todos, { text: value }];
+
+    setTodos(addedTodo);
+  };
+
+  console.log("todos", todos);
+
   return (
     <Paper>
       <Header />
-      <TodoForm />
+      <TodoForm addTodo={addTodo} />
 
       <Todos todos={todos} />
     </Paper>
